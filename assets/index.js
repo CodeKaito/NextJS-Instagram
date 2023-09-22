3a1e05401c99bdeeee9edf7a7741ffc0bd4d38db
CodeKaito <jerome.decinco@mawell.mi.it>
2023-09-22 13:50:25 +0200

    ✅ Upload to production

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,5 @@
 import logo from './Logo-Instagram.png';
+import logosmall from './instagram-logo-small.png';
 
 export { logo };
+export { logosmall };
--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,32 @@
 import Image from 'next/image'
 import React from 'react'
-import { logo } from '@/assets'
+import { logo, logosmall } from '@/assets'
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon
+} from '@heroicons/react/outline';
 
 const Header = () => {
   return (
     <div>
         <div className='flex justify-between max-w-6xl'>
             {/* LeftSide */}
-            <div className='relative h-24 w-24'>
+            <div className='relative hidden lg:inline-grid h-24 w-24'>
                 <Image src={logo} objectFit='contain' className='cursor-pointer' />
             </div>
 
+            <div className='relative w-10 h-10 lg:hidden flex-shrink-0'>
+              <Image src={logosmall} objectFit='contain' className='cursor-pointer' />
+            </div>
+
             {/* MiddleSide */}
+            <div>
+              <input type='text' placeholder='Search' className='hidden' />
+            </div>
 
             {/* RightSide */}
         </div>
