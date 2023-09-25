d4eb96cd92de7d1d8608a4a5d6c9d85c7c80a887
CodeKaito <jerome.decinco@mawell.mi.it>
2023-09-25 14:50:39 +0200

    ✅ Upload to production

---
+++ components/Posts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Posts = () => {
+  return (
+    <div>I am a post</div>
+  )
+}
+
+export default Posts
--- components/Story.js
+++ components/Story.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Story = ({ img, username } ) => {
   return (
     <div>
-        <img className='h-14 w-14 rounded-full p-[1px] border-red-500 border-2 object-contain cursor-pointer' src={img} alt='' />
-        <p className='text-xs'>{username}</p>
+        <img className='h-14 w-14 rounded-full p-[1px] border-red-500 border-2 object-contain cursor-pointer hover:scale-110 transition transform duration-200 ease-out' src={img} alt='' />
+        <p className='text-xs w-14 truncate text-center'>{username}</p>
     </div>
   )
 }
